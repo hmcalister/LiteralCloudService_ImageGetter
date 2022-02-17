5eeeda6d0e6d7c2aa225c7a04fd08777e1486675
Hayden McAlister <haydenmcalister49@gmail.com>
2022-02-18 06:03:45 +1300

    Add logging header to get sources script

--- get_sources.py
+++ get_sources.py
@@ -1,2 +1,10 @@
 from Clouds import CloudSource
+import logging, datetime
+logging.info("-"*80)
+logging.info("*"*80)
+logging.info(f"SCRIPT START AT LOCAL {datetime.datetime.now()}")
+logging.info(f"SCRIPT START AT UTC {datetime.datetime.utcnow()}")
+logging.info("*"*80)
+logging.info("-"*80)
+
 CloudSource.wget_sources()
