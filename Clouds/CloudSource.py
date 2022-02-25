4398a49f9b81b6c07301dd69052fc5ba885f9708
Hayden McAlister <haydenmcalister49@gmail.com>
2022-02-26 00:19:51 +1300

    Shift get sources script to get_sources.py
    This allows for better altering of default params

--- Clouds/CloudSource.py
+++ Clouds/CloudSource.py
@@ -14,11 +14,6 @@ from PIL import Image, UnidentifiedImageError
 
 # To get around dodgy webcams with expired SSL certificates
 ssl._create_default_https_context = ssl._create_unverified_context
-logging.Formatter.converter = time.gmtime
-logging.basicConfig(filename=f"logs/{str(datetime.datetime.utcnow().date())}-cloud-sources.log", 
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
 
 class CloudSource:
     """
@@ -320,65 +315,3 @@ def archive_images(backup_dir=None, current_dir="images/current_downloads"):
         logging.info("ERROR: Failed to move images to backup folder!")
         logging.debug(f"{e=}")
         logging.debug(f"{type(e)=}")
-
-def wget_sources():
-    """
-    For all of the cloud sources in the cloud source json file, get the image from that source for this day.
-
-    This is currently very static, intended to get the job done quickly. In future this may be parameterized.
-    """
-
-    logging.info("LOAD SOURCES")
-    cloud_sources:List[CloudSource] = get_cloud_sources()
-    logging.info("LOAD SUCCESSFUL")
-    logging.debug(cloud_sources)
-    logging.info("-"*80)
-
-    # logging.info("GET TARGET TIMES")
-    # for source in cloud_sources:
-    #     now = datetime.datetime.utcnow()
-    #     logging.debug(f"{now=}")
-    #     logging.debug(f"{source=}")
-    #     source.set_target_time(now)
-
-    # logging.info("TARGET TIMES CREATED")
-    # logging.info("-"*80)
-    logging.info("SORTING SOURCES")
-    cloud_sources.sort(key = lambda x:x.target_time)
-    logging.info("SOURCES SORTED")
-    for source in cloud_sources:
-        logging.info(f"{str(source):<50}{source.target_time}")
-
-    logging.info("-"*80)
-
-    logging.info("START GET FROM SOURCES")
-    try:
-        for source in cloud_sources:
-            logging.info(f"{source=}")
-            logging.info(f"CURRENT TIME (LOCAL): {datetime.datetime.now()}")
-            logging.info(f"CURRENT TIME (UTC): {datetime.datetime.utcnow()}")
-            now = datetime.datetime.utcnow()
-            delta = source.target_time - now
-            if delta.total_seconds() > 0:
-                logging.info(f"SLEEPING FOR {delta.total_seconds()}s")
-                logging.info(f"SLEEP FINISH SCHEDULED FOR (LOCAL): {datetime.datetime.now()+delta}")
-                logging.info(f"SLEEP FINISH SCHEDULED FOR (UTC): {datetime.datetime.utcnow()+delta}")
-                time.sleep(delta.total_seconds())
-            else:
-                logging.info(f"NO SLEEP NEEDED")
-            logging.info(f"GET {str(source)}")
-            if source.get_image():
-                logging.info("GET SUCCESSFUL")
-            else:
-                logging.info("GET FAILED")
-            logging.info("-"*80)
-        logging.info("ALL SOURCES GET FINISHED")
-        logging.info("-"*80)
-    except KeyboardInterrupt:
-        logging.info("*"*80)
-        logging.info("KEYBOARD INTERRUPT")
-        logging.info("ARCHIVE CURRENT IMAGES")
-        logging.info("*"*80)
-    logging.info("MOVE IMAGES TO BACKUP FOLDER")
-    archive_images()
-    logging.info("-"*80)
--- get_sources.py
+++ get_sources.py
@@ -1,5 +1,13 @@
+import logging, datetime, time
+
+logging.Formatter.converter = time.gmtime
+logging.basicConfig(filename=f"logs/{str(datetime.datetime.utcnow().date())}-cloud-sources.log", 
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
 from Clouds import CloudSource
-import logging, datetime
+
 logging.info("-"*80)
 logging.info("*"*80)
 logging.info(f"SCRIPT START AT LOCAL {datetime.datetime.now()}")
@@ -7,4 +15,58 @@ logging.info(f"SCRIPT START AT UTC {datetime.datetime.utcnow()}")
 logging.info("*"*80)
 logging.info("-"*80)
 
-CloudSource.wget_sources()
+logging.info("LOAD SOURCES")
+cloud_sources = CloudSource.get_cloud_sources()
+logging.info("LOAD SUCCESSFUL")
+logging.debug(cloud_sources)
+logging.info("-"*80)
+
+# logging.info("GET TARGET TIMES")
+# for source in cloud_sources:
+#     now = datetime.datetime.utcnow()
+#     logging.debug(f"{now=}")
+#     logging.debug(f"{source=}")
+#     source.set_target_time(now)
+
+# logging.info("TARGET TIMES CREATED")
+# logging.info("-"*80)
+
+logging.info("SORTING SOURCES")
+cloud_sources.sort(key = lambda x:x.target_time)
+logging.info("SOURCES SORTED")
+for source in cloud_sources:
+    logging.info(f"{str(source):<50}{source.target_time}")
+
+logging.info("-"*80)
+
+logging.info("START GET FROM SOURCES")
+try:
+    for source in cloud_sources:
+        logging.info(f"{source=}")
+        logging.info(f"CURRENT TIME (LOCAL): {datetime.datetime.now()}")
+        logging.info(f"CURRENT TIME (UTC): {datetime.datetime.utcnow()}")
+        now = datetime.datetime.utcnow()
+        delta = source.target_time - now
+        if delta.total_seconds() > 0:
+            logging.info(f"SLEEPING FOR {delta.total_seconds()}s")
+            logging.info(f"SLEEP FINISH SCHEDULED FOR (LOCAL): {datetime.datetime.now()+delta}")
+            logging.info(f"SLEEP FINISH SCHEDULED FOR (UTC): {datetime.datetime.utcnow()+delta}")
+            time.sleep(delta.total_seconds())
+        else:
+            logging.info(f"NO SLEEP NEEDED")
+        logging.info(f"GET {str(source)}")
+        if source.get_image():
+            logging.info("GET SUCCESSFUL")
+        else:
+            logging.info("GET FAILED")
+        logging.info("-"*80)
+    logging.info("ALL SOURCES GET FINISHED")
+    logging.info("-"*80)
+except KeyboardInterrupt:
+    logging.info("*"*80)
+    logging.info("KEYBOARD INTERRUPT")
+    logging.info("ARCHIVE CURRENT IMAGES")
+    logging.info("*"*80)
+logging.info("MOVE IMAGES TO BACKUP FOLDER")
+CloudSource.archive_images()
+logging.info("-"*80)
