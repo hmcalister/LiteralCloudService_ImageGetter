983fa966f4b5fc8a0339e2120e3303b937ae00f5
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-10 00:13:23 +1300

    Add new testing urls

--- test_sources.py
+++ test_sources.py
@@ -43,8 +43,30 @@ logging.info("-"*80)
 
 # Map of names to urls to test
 CLOUD_SOURCE_URLS = {
-    "BridgemanTaranaki-NZ": "https://www.primo.nz/webcameras/snapshot_bridgeman.jpg",
-    "TuahuTaranaki-NZ": "https://www.primo.nz/webcameras/snapshot_tuahu.jpg",
+    "Kalbarri Australia": "https://mywebcams.com.au/WA/Kalbarri.php",
+    "GearldtonNE Australia": "https://view.myairportcams.com/YGEL/showimagecamera1.php",
+    "GearldtonSE Australia": "https://view.myairportcams.com/YGEL/showimagecamera4.php",
+    "JurienHarbour Australia": "https://mywebcams.com.au/WA/JurienHarbour.php",
+    "Lancelin Australia": "https://mywebcams.com.au/WA/Lancelin.php",
+    "Swanbourne Australia": "https://mywebcams.com.au/WA/Swanbourne.php",
+    "Freemantle Australia": "https://mywebcams.com.au/WA/Fremantle.php",
+    "Bunbury Airport Australia": "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/Bunbury_Airport/Bunbury_Airport_340.jpg",
+    "Augusta Australia": "https://view.myairportcams.com/YAUG/showimagecamera1.php",
+    "Peaceful Australia": "https://mywebcams.com.au/WA/Peaceful.php",
+    "AlbanyNE Western Australia": "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/009999/009999_225.jpg",
+    "AlbanySW Western Australia": "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/009999/009999_045.jpg",
+    "Esperance Australia": "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/009542/009542_225.jpg",
+    "Jacinth Ambrosia Australia": "https://view.myairportcams.com/YJAC/showimagecamera1.php",
+    "Ceduna Australia": "https://view.myairportcams.com/YCDU/showimagecamera1.php",
+    "Cairns Australia": "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/031011/031011_180.jpg",
+    "Archerfield Airport Australia": "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/040211/040211_225.jpg",
+    "Gold Coast Australia": "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/040717/040717_360.jpg",
+    "Toowomba Airport Australia": "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/041529/041529_045.jpg",
+    "Narrandera Australia": "https://assets3.webcam.io/w/z5G0w9/latest_hd.jpg",
+    "Wagga Wagga Australia": "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/072150/072150_135.jpg",
+    "Hamilton East Mountains Australia": "https://view.myairportcams.com/YHML/eastmtn.php",
+    "Warrnambool Australia": "https://view.myairportcams.com/YWBL/showimagecamera1.php",
+    "Tintinara Australia": "https://view.myairportcams.com/OZTIA/showimagecamera1.php",
 }
 logging.info("TEST SOURCES")
 for name, url in CLOUD_SOURCE_URLS.items():
