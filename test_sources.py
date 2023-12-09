2c565c39445462ba0ab28074ec270df14e8a645f
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-09 23:28:32 +1300

    Update test_sources with template to test new urls

--- test_sources.py
+++ test_sources.py
@@ -14,15 +14,30 @@ logging.info(f"SCRIPT START AT UTC {datetime.datetime.utcnow()}")
 logging.info("*"*80)
 logging.info("-"*80)
 
-logging.info("LOAD SOURCES")
-with open("Clouds/CloudSourcesData.json", "r") as f:
-    cloudSourcesJSON = json.load(f)
-logging.info("LOAD SUCCESSFUL")
+# logging.info("LOAD SOURCES")
+# with open("Clouds/CloudSourcesData.json", "r") as f:
+#     cloudSourcesJSON = json.load(f)
+# logging.info("LOAD SUCCESSFUL")
 
+# logging.info("TEST SOURCES")
+# for sourceJSON in cloudSourcesJSON["CloudSources"]:
+#     logging.info("-"*80)
+#     source = CloudSource.CloudSource(sourceJSON["name"], sourceJSON["url"], literal_eval(sourceJSON["crop_coords"]), "00:00")
+#     logging.info(f"TESTING SOURCE {source.name} STARTED")
+#     if source.get_image(directory="images/test_images"):
+#         logging.info("GET SUCCESSFUL")
+#     else:
+#         logging.info("GET FAILED")
+#     logging.info("-"*80)
+
+# Map of names to urls to test
+CLOUD_SOURCE_URLS = {
+    
+}
 logging.info("TEST SOURCES")
-for sourceJSON in cloudSourcesJSON["CloudSources"]:
+for name, url in CLOUD_SOURCE_URLS.items():
     logging.info("-"*80)
-    source = CloudSource.CloudSource(sourceJSON["name"], sourceJSON["url"], literal_eval(sourceJSON["crop_coords"]), "00:00")
+    source = CloudSource.CloudSource(name, url, None, "00:00")
     logging.info(f"TESTING SOURCE {source.name} STARTED")
     if source.get_image(directory="images/test_images"):
         logging.info("GET SUCCESSFUL")
